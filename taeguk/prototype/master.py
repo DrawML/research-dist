f6b412a7bb62f20221ee71c4056792c7f75559d1
taeguk <xornrbboy@gmail.com>
2016-08-02 16:07:47 +0900

    change naming from worker to slave and tiny draft for connection scenario.

--- taeguk/prototype/master.py
+++ taeguk/prototype/master.py
@@ -9,21 +9,33 @@ Assumption :
 
 """
 Disconnection process will be added....
+
+TODO:
+1. connection management of Client.
+2. `` of Slave.
+
+master : 분산 시스템의 두목
+slave : task를 할당받는 자
+client : task를 던지는 자.
+
+근데 어차피 셋 다 전부 내부 시스템이 될 것임.
+굳히 다양하게 연결불능까지 처리해야할까??
+
+시나리오)
+1. slave와 통신이 잘 안 될 때,
+    slave의 상테를 통신불능으로 표시해 task가 할당되지 못하게 한다.
+2. task를 할당받은 slave와 통신이 잘 안 될 떼,
+    특정 timeout만큼 통신되길 기다렸다가, 그래도 실패하면, task를 다시 task queue에 넣고,
+    slave에게 표시를 해둔다.
+3. 일정 시간 이상 slave와 통신불능일 떼,
+    slave와 통신이 영구적으로 끊켰다고 보고, slave와의 연결을 해제하고 관련 처리를 한다.
+4. client와 통신이 잘 안 될때, 일정시간 이상 client와 통신 불능일 때,
+    slave와 비슷한 방식으로 처리.
+5. client에게 task result를 response해야하는데, 통신불능일 떼,
+    일단 임시로 '미전달 result 목록'에 넣어놓고 추후에 통신이 가능해졌을 떼 전달해준다.
 """
 
 import zmq
-
-# Prepare our context and sockets
-context = zmq.Context()
-sock_client = context.socket(zmq.ROUTER)
-sock_worker = context.socket(zmq.ROUTER)
-sock_client.bind("tcp://*:5559")
-sock_worker.bind("tcp://*:5560")
-
-# Initialize poll set
-poller = zmq.Poller()
-poller.register(sock_client, zmq.POLLIN)
-poller.register(sock_worker, zmq.POLLIN)
 
 class Client(object):
     def __init__(self, addr):
@@ -35,6 +47,14 @@ class Client(object):
     @property
     def addr(self):
         return self._addr
+
+
+class ClientManager(object):
+    def __init__(self):
+        self._clients = []
+
+    def add_client(self, client):
+        pass
 
 
 class Task(object):
@@ -64,7 +84,15 @@ class Task(object):
         return self._data
 
 
-class Worker(object):
+class TaskManager(object):
+    def __init__(self):
+        self._tasks = []
+
+    def add_task(self):
+        pass
+
+
+class Slave(object):
     def __init__(self, addr):
         self._addr = addr
         self._no = None
@@ -74,7 +102,7 @@ class Worker(object):
         return self._addr == other._addr
 
     def do_task(self, task):
-        sock_worker.send_multipart([self.addr, b'', task.data])
+        slave_router.send_multipart([self.addr, b'', task.data])
         self.doing_task = task
 
     @property
@@ -98,47 +126,47 @@ class Worker(object):
         self._doing_task = task
 
 
-class WorkerManager(object):
+class SlaveManager(object):
     def __init__(self):
-        self._workers = []
+        self._slaves = []
 
-    def add_worker(self, worker):
-        if self.check_worker_existence(worker):
+    def add_slave(self, slave):
+        if self.check_slave_existence(slave):
             # error.
-            raise ValueError("Duplicated Worker.")
+            raise ValueError("Duplicated Slave.")
         else:
-            worker.no = self.count()
-            self._workers.append(worker)
+            slave.no = self.count()
+            self._slaves.append(slave)
 
-    def del_worker(self, worker):
-        if self.check_worker_existence(worker):
-            self._workers.remove(worker)
+    def del_slave(self, slave):
+        if self.check_slave_existence(slave):
+            self._slaves.remove(slave)
         else:
             # error.
-            raise ValueError("Non-existent Worker.")
+            raise ValueError("Non-existent Slave.")
 
-    def check_worker_existence(self, worker):
-        return worker in self._workers
+    def check_slave_existence(self, slave):
+        return slave in self._slaves
 
     def count(self):
-        return len(self._workers)
+        return len(self._slaves)
 
-    # Get available worker for processing task.
-    def get_worker(self, task):
+    # Get available slave for processing task.
+    def get_slave(self, task):
         # some algorithms will be filled in here.
-        available_workers = [w for w in self._workers if w.doing_task is None]
-        if not available_workers:
-            raise Exception("Not available Workers.")
+        available_slaves = [w for w in self._slaves if w.doing_task is None]
+        if not available_slaves:
+            raise Exception("Not available Slaves.")
         import random
-        worker = random.choice(available_workers)
-        return worker
+        slave = random.choice(available_slaves)
+        return slave
 
-    def find_worker(self, worker):
-        if self.check_worker_existence(worker):
-            return self._workers[self._workers.index(worker)]
+    def find_slave(self, slave):
+        if self.check_slave_existence(slave):
+            return self._slaves[self._slaves.index(slave)]
         else:
             # error.
-            raise ValueError("Non-existent Worker.")
+            raise ValueError("Non-existent Slave.")
 
 class TaskQueue(object):
     def __init__(self):
@@ -186,9 +214,9 @@ class TaskQueue(object):
             task = self.top()
 
             try:
-                worker = worker_manager.get_worker(task)
+                slave = slave_manager.get_slave(task)
                 self.pop()
-                worker.do_task(task)
+                slave.do_task(task)
                 print("[*] Task Queue Size =", self.count())
             except Exception as err:
                 print("[!]", err)
@@ -196,20 +224,18 @@ class TaskQueue(object):
         except ValueError as err:
             print("[!]", err)
 
-task_queue = TaskQueue()
-worker_manager = WorkerManager()
 
 def communicate_with_client():
-    address, empty, message = sock_client.recv_multipart()
+    address, empty, message = client_router.recv_multipart()
     client = Client(address)
     task = Task(message, client)
     task_queue.push(task)
     task_queue.process()
 
-def communicate_with_worker():
-    address, empty, message = sock_worker.recv_multipart()
+def communicate_with_slave():
+    address, empty, message = slave_router.recv_multipart()
 
-    worker = Worker(address)
+    slave = Slave(address)
 
     # will be modified.
     header = message[0:10]
@@ -217,19 +243,19 @@ def communicate_with_worker():
 
     if header == b'ready_____':
         try:
-            worker_manager.add_worker(worker)
-            print("[*] Worker {0} is ready!".format(worker.no))
+            slave_manager.add_slave(slave)
+            print("[*] Slave {0} is ready!".format(slave.no))
             task_queue.process()
         except ValueError as err:
             print("[!]", err)
 
     elif header == b'finish____':
-        real_worker = worker_manager.find_worker(worker)
-        print("[*] Worker {0} is finished!".format(real_worker.no))
-        task = real_worker.doing_task
+        real_slave = slave_manager.find_slave(slave)
+        print("[*] Slave {0} is finished!".format(real_slave.no))
+        task = real_slave.doing_task
         client = task.client
-        sock_client.send_multipart([client.addr, b'', body])
-        real_worker.doing_task = None
+        client_router.send_multipart([client.addr, b'', body])
+        real_slave.doing_task = None
         task_queue.process()
 
     elif header == b'exit______':
@@ -238,15 +264,30 @@ def communicate_with_worker():
     else:
         raise ValueError("Unknown Message.")
 
+# Prepare our context and sockets
+context = zmq.Context()
+client_router = context.socket(zmq.ROUTER)
+slave_router = context.socket(zmq.ROUTER)
+client_router.bind("tcp://*:5559")
+slave_router.bind("tcp://*:5560")
+
+# Initialize poll set
+poller = zmq.Poller()
+poller.register(client_router, zmq.POLLIN)
+poller.register(slave_router, zmq.POLLIN)
+
+task_queue = TaskQueue()
+slave_manager = SlaveManager()
+
 # Switch messages between sockets
 while True:
     print("loop!")
     socks = dict(poller.poll())
 
-    if socks.get(sock_client) == zmq.POLLIN:
-        print("[*] sock_client pollin!")
+    if socks.get(client_router) == zmq.POLLIN:
+        print("[*] client_router pollin!")
         communicate_with_client()
 
-    if socks.get(sock_worker) == zmq.POLLIN:
-        print("[*] sock_worker pollin!")
-        communicate_with_worker()
+    if socks.get(slave_router) == zmq.POLLIN:
+        print("[*] slave_router pollin!")
+        communicate_with_slave()
